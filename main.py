c1771dbfc94e25540b3a991cd9f8789ab0a6a4f4
Maxmilian Mázek <blockmagix97@gmail.com>
2022-09-23 21:22:52 +0200

    v1.0

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from tkinter import *
+import sys
 
-root = Tk()
+def start():
+    print("\n Type esc to exit\n")
+    while True:
+        guess = input("Rock(r), Paper(p), Scissors(s): ")
+        if guess == "esc".lower():
+            print("\n----------------\n\nGAME OVER")
+            sys.exit()
 
-
-
-root.mainloop()
+start()
